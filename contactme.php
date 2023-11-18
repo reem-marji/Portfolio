4c0976450cc642ddf2a5f4c915fd1b49105c2f41
REEM-LAPTOP\user <reem.marji2112@gmail.com>
2023-11-19 00:28:27 +0200

    adding a final contact me page

---
+++ contactme.php
@@ -0,0 +1,107 @@
+    <?php include('header.php'); ?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <style>
+            body{
+            background-color: #f8fbf9;
+            margin: 0%;
+            font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+            background-color: #f0fbf4;
+        }
+
+        .container {
+            display: flex;
+            flex-direction: row;
+            margin: auto;
+            width: 50%;
+            padding: 10px;
+            }
+
+        .box {
+            margin: 25px;
+            margin-top: 40px;
+            padding: 20px;
+            background-color: #fff;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+            border-radius: 2%;
+        }
+
+        p {
+            line-height: 1.5;
+        }
+
+        .middle-container{
+            margin: 100px 0px;
+            text-align: center;
+        }
+
+        .contact-message{
+            width: 40%;
+            margin: 40px auto 60px;
+        }
+
+        .btn {
+            background: #40a474;
+            -webkit-border-radius: 8;
+            -moz-border-radius: 8;
+            border-radius: 8px;
+            font-family: Arial;
+            color: #ffffff;
+            font-size: 20px;
+            padding: 10px 20px 10px 20px;
+            text-decoration: none;
+        }
+        
+        .btn:hover {
+            background: #8bff87;
+            text-decoration: none;
+        }
+
+        .bottom-container{
+            margin: 0; 
+            background-color: #40a474;
+            padding: 50px 0 20px;
+            text-align: center;
+        }
+
+        .copyright{
+            color: #EAF6F6;
+            font-size: 0.75rem;
+            padding: 20px 0px;
+        }
+
+        .contact-me h2, h4, p {
+            margin: 0; 
+            padding-top: 10px;
+            color: #40a474;
+        }
+
+
+        </style>        
+    </head>
+
+    <body>
+    
+        <div class="middle-container">
+            <div class="contact-me">
+                <h2>Get In Touch</h1>
+                <h4>Kindly find my contact details below.</h4>
+                <p>For Job/Internship opportunities or freelance projects</p>
+                <br></br>
+                <a class="btn" href="mailto:reem.marji2112@gmail.com">CONTACT ME</a>
+            </div>
+
+        </div>
+
+
+    <div class="bottom-container">
+        <p class="copyright" >© 2023 Reem Marji</p>
+    </div>
+    
+
+    </body>
+
+    </html>
